6ebaf2209dbbac3e064a81060313f6eab0fd59a3
unknown <dongwoo.jeong@outlook.com>
2022-02-02 23:50:43 -0500

    add iris

---
+++ iris.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import pandas as pd
+
+파일경로 = 'https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/iris.csv'
+아이리스 = pd.read_csv(파일경로)
+
+아이리스 = pd.get_dummies(아이리스)
+
+독립 = 아이리스[['꽃잎길이', '꽃잎폭', '꽃받침길이', '꽃받침폭']]
+종속 = 아이리스[['품종_setosa', '품종_versicolor', '품종_virginica']]
+print(독립.shape, 종속.shape)
+
+X = tf.keras.layers.Input(shape=[4])
+H = tf.keras.layers.Dense(8, activation="swish")(X)
+H = tf.keras.layers.Dense(8, activation="swish")(H)
+H = tf.keras.layers.Dense(8, activation="swish")(H)
+Y = tf.keras.layers.Dense(3, activation='softmax')(H)
+model = tf.keras.models.Model(X, Y)
+model.compile(loss='categorical_crossentropy',
+              metrics='accuracy')
+
+model.summary()
+
+model.fit(독립, 종속, epochs=100)
+
+print(model.predict(독립[:5]))
+print(종속[:5])
